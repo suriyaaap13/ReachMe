c9b1bfa8c705126772ae0e7152bc2a4db20aad76
Suriyaa <suriyaaap13@gmail.com>
2022-03-27 12:34:57 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+app.listen(port, function(err){
+    if(err){
+        console.log(`Error: ${err}`);
+    }else{
+        console.log(`Server is up and running in port ${port}`);
+    }
+});
