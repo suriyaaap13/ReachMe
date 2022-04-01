447786a3b7f33ca97141f5a4c809a233bbaf7608
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-01 20:42:50 +0530

    populated the comments, users of the comment and displayed them in the home page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,15 @@
 const { is } = require('express/lib/request');
 const Post = require('../models/post');
 module.exports.home = (req,res)=>{
-    Post.find({}).populate('user').exec(function(err, posts){
+    Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    })
+    .exec(function(err, posts){
         if(err){console.log("Error in finding the post");return;}
         return res.render('home', {
             title: "home",
