52d96402d36d60a1c844d8ffb12c73e9e7e84899
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-01 14:31:02 +0530

    populate user and display the username in each post

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -3,13 +3,15 @@ const Post = require('../models/post');
 module.exports.home = (req,res)=>{
 
     if(req.isAuthenticated()){
-        Post.find({},function(err, posts){
-            if(err){console.log("Error in finding the post");}
+        
+        Post.find({}).populate('user').exec(function(err, posts){
+            if(err){console.log("Error in finding the post");return;}
             return res.render('home', {
                 title: "home",
                 post: posts
             });
-        })
+        });
+
     }else{
         return res.render('home', {
             title: "home"
