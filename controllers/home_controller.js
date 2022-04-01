0a5d79f0cbeec2b85d6be6d4728d5316025bd045
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-01 14:37:38 +0530

    use passport to protect from illegal posts

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,21 +1,11 @@
 const { is } = require('express/lib/request');
 const Post = require('../models/post');
 module.exports.home = (req,res)=>{
-
-    if(req.isAuthenticated()){
-        
-        Post.find({}).populate('user').exec(function(err, posts){
-            if(err){console.log("Error in finding the post");return;}
-            return res.render('home', {
-                title: "home",
-                post: posts
-            });
-        });
-
-    }else{
+    Post.find({}).populate('user').exec(function(err, posts){
+        if(err){console.log("Error in finding the post");return;}
         return res.render('home', {
-            title: "home"
+            title: "home",
+            post: posts
         });
-    }
-    
+    });
 }
