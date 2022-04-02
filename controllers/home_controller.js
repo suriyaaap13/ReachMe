b9ad54de20dbd8add63c756c40e0eba722dd3874
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-02 23:01:07 +0530

    style the login page

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,23 +2,27 @@ const User = require('../models/user');
 const Post = require('../models/post');
 
 module.exports.home = (req,res)=>{
-    Post.find({})
-    .populate('user')
-    .populate({
-        path: 'comments',
-        populate: {
-            path: 'user'
-        }
-    })
-    .exec(function(err, posts){
-        if(err){console.log("Error in finding the post");return;}
-        User.find({}, function(err, users){
-            return res.render('home', {
-                title: "home",
-                post: posts,
-                all_users: users
+    if(res.locals!=null){
+        Post.find({})
+        .populate('user')
+        .populate({
+            path: 'comments',
+            populate: {
+                path: 'user'
+            }
+        })
+        .exec(function(err, posts){
+            if(err){console.log("Error in finding the post");return;}
+            User.find({}, function(err, users){
+                return res.render('home', {
+                    title: "home",
+                    post: posts,
+                    all_users: users
+                });
             });
+            
         });
-        
-    });
+    }else{
+        res.redirect('/users/login');
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const passport = require('passport');
 
 
 const homeController = require('../controllers/home_controller');
 
 
 
-router.get('/', homeController.home);
+router.get('/',homeController.home);
 router.use('/users', require('./users'));
 router.use('/posts', require('./posts'));
 router.use('/comments', require('./comment'));
