d722f042400c053fbdbe240878110a3904776087
suriyaaap13 <suriyaaap13@gmail.com>
2022-04-02 09:00:50 +0530

    Implement delete post and all the comments in the post respective post

--- controllers/post_controller.js
+++ controllers/post_controller.js
@@ -1,4 +1,5 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = function(req, res){
     console.log(req.body.content);
@@ -13,4 +14,23 @@ module.exports.create = function(req, res){
             return res.redirect('back');
         });
     }
+}
+
+module.exports.destroy = function(req, res){
+    // id means converting the objectId to string if _id is put then undefined will be shown.
+    Post.findById(req.params.id, function(err, post){
+        if(err){console.log("Error in finding the post");return;}
+        // post.user._id = undefined req.locals._id is also undefined
+        // console.log(post.user.id+" "+post.user.id+" "+req.locals._id);
+        if(post.user == req.user.id){
+            post.remove();
+            Comment.deleteMany({post: req.params.id}, function(err){
+                if(err){console.log("Error in deleting all the comments "+err);return;}
+                return res.redirect('back');
+            })
+        }else{
+            return res.redirect('back');
+        }
+        
+    });
 }
--- routes/posts.js
+++ routes/posts.js
@@ -5,4 +5,5 @@ const router = express.Router();
 const postController = require('../controllers/post_controller');
 
 router.post('/create', passport.checkAuthentication, postController.create);
+router.get('/destroy/:id', passport.checkAuthentication, postController.destroy);
 module.exports = router;
